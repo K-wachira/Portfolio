430d361c9c1ee4911f6ad33017c8c65569737b7f
k-wachira <abkelvinwachira@gmail.com>
2022-10-05 10:22:36 +0300

    remove underline in the contact button, added email to the contact page

--- client/src/Components/Users/Home/Contact.jsx
+++ client/src/Components/Users/Home/Contact.jsx
@@ -2,12 +2,22 @@ import React from "react";
 
 function Contact() {
   return (
-    <div className="container parentbanner">
-      <p>
-        Whether it's an interesting idea for a project that you have, some
-        consultancy work you may need, or just a quick hello, I'd love to chat!
-        I typically respond within a few hours.
-      </p>
+    <div>
+      <div className="container contactbanner ">
+        <div className="fs-1 font-weight-bold mb-5">GET IN TOUCH</div>
+        <div>
+          <p className="contact">
+            Whetherit's an interesting idea for a project that you have, some
+            consultancy work you may need, or just a quick hello, I'd love to
+            chat! I typically respond within a few hours.
+          </p>
+          <div></div>
+          Please reach out to{" "}
+          <a href="mailto:abkelvinwachira@gmail.com" className="">
+            abkelvinwachira@gmail.com
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
