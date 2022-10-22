51a4bc86645f4ea45ba3b813de0d68b78a6aa784
k-wachira <abkelvinwachira@gmail.com>
2022-10-22 10:45:50 +0300

    cover image upload functinality

--- client/src/Components/Admin/Posts/TitleForm.jsx
+++ client/src/Components/Admin/Posts/TitleForm.jsx
@@ -1,23 +1,49 @@
 import React from "react";
 import { useState } from "react";
 import TagsInput from "react-tagsinput";
+import axios from "axios";
 
 import "react-tagsinput/react-tagsinput.css";
 
 function Forms(props) {
-  console.log(Object.values(props.tags));
   const [title, setTitle] = useState(props.title);
   const [description, setDescription] = useState(props.description);
   const [tags, setTags] = useState(Object.values(props.tags));
   const [category, setCategory] = useState(props.category);
+  const [selectedFile, setSelectedFile] = useState();
+  const [isSelected, setIsSelected] = useState(false);
+
   const handleSubmit = (e) => {
-    props.handleForm({ title, description, tags, category });
-    e.preventDefault();
+    if (!isSelected) {
+      props.handleForm({ title, description, tags, category });
+      e.preventDefault();
+    }
+  };
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
+
+  const handleSubmission = () => {
+    const formData = {"uploaded_file": selectedFile, post_id: "635159869e6e5abdcfe0a33b"};
+    axios
+      .post("http://localhost:5000/api/v1/blog/upload-cover", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+      setIsSelected(false)
   };
 
   return (
-    <div className="container bg-light">
+    <div className="bg-light p-4">
       <form
+        className=""
         onSubmit={(e) => {
           handleSubmit(e);
         }}
@@ -25,41 +51,58 @@ function Forms(props) {
         <div className="form-group">
           <label>Title</label>
           <input
-            className="form-control"
+            className="form-control mt-2"
             name="title"
             type="text"
             value={title}
             onChange={(event) => setTitle(event.target.value)}
           />
         </div>
-        <div className="form-group">
-          <label>Description:</label>
+        <div className="form-group mt-4">
+          <label>Description</label>
           <textarea
-            className="form-control"
+            className="form-control mt-2"
             name="description"
             type="text-area"
             value={description}
             onChange={(event) => setDescription(event.target.value)}
           />
         </div>
-        <div className="form-group">
-          <label>Tags:</label>
-          <TagsInput value={tags} onChange={setTags} />
+        <div className="mt-4">
+          <label>Tags</label>
+          <TagsInput
+            className="form-control mt-2"
+            value={tags}
+            onChange={setTags}
+          />
         </div>
-        <div className="form-group">
+        <div className="form-group mt-4">
           <label>Category:</label>
           <input
-            className="form-control"
+            className="form-control mt-2"
             name="category"
             type="text"
             value={category}
             onChange={(event) => setCategory(event.target.value)}
           />
         </div>
-        <div className="actions m-4 d-grid gap-2">
+
+        <div>
+          <input type="file" name="file" onChange={changeHandler} />
+          {isSelected ? (
+            <div>
+              <p>Size in bytes: {selectedFile.size / 1000}</p>
+
+              <button onClick={handleSubmission}>Submits</button>
+            </div>
+          ) : (
+            <p>Select a file to show details</p>
+          )}
+        </div>
+        <div className="actions mt-4 d-grid">
           <input
             type="submit"
-            value="Submit"
+            value="Update"
             className="btn btn-primary btn-block"
           />
         </div>
