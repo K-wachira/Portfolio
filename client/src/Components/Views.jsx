c914ae1c2f4445616d057fc07eda4ee73d98ec5a
k-wachira <abkelvinwachira@gmail.com>
2022-10-22 10:31:12 +0300

    Renamed the about, allposts, edit post pages

--- client/src/Components/Views.jsx
+++ client/src/Components/Views.jsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes  } from "react-router-dom";
 import Home from "./Users/Home/Home.jsx";
-import Contact from "./Users/Home/Contact.jsx";
-import Posts from "./Admin/Posts/index.jsx";
-import EditPost from "./Admin/Posts/edit.jsx";
+import About from "./Users/Home/About.jsx";
+import Posts from "./Admin/Posts/AllPosts.jsx";
+import EditPost from "./Admin/Posts/EditPost.jsx";
 
 import PageNotFound from "./Shared/404.jsx";
 import UnderContruction from "./Shared/Underdevelopment.jsx";
@@ -13,9 +12,9 @@ const Views = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Blog" element={<UnderContruction />} />
-      <Route path="/LetsChat" element={<Contact />} />
+      <Route path="/About" element={<About />} />
       <Route path="/Posts" element={<Posts />} />
-      <Route path="/editPost/:post_id" element={<EditPost  />} />
+      <Route path="/editPost/:post_id/edit" element={<EditPost  />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
