7289c14a097b358a27fd9e7cdbc5b4b467660edc
k-wachira <abkelvinwachira@gmail.com>
2022-10-22 10:27:26 +0300

    Added publish and unpublish toggle, upload of image cover api and refactored delete post service

--- client/src/Components/Admin/Services/posts.service.js
+++ client/src/Components/Admin/Services/posts.service.js
@@ -1,34 +1,43 @@
 import http from "../../../http-common";
 
 class PostDataService {
-    getAll(page = 0) {
-      return http.get(`all_posts?page=${page}`);
-    }
-  
-    get(id) {
-      return http.get(`/post_by_id?post_id=${id}`);
-    }
-  
-    find(query, by = "name", page = 0) {
-      return http.get(`posts?${by}=${query}&page=${page}`);
-    } 
-  
-    createPost() {
-      return http.post("/post");
-    }
-  
-    updatePost(data) {
-      return http.put("/post", data);
-    }
-  
-    deletePost(id, userId) {
-      return http.delete(`/post?post_id=${id}`, {data:{user_id: userId}});
-    }
-  
-    getCategories(id) {
-      return http.get(`/categories`);
-    }
-  
+  getAll(page = 0) {
+    return http.get(`all_posts?page=${page}`);
   }
-  
-  export default new PostDataService();
+
+  get(id) {
+    return http.get(`/post_by_id?post_id=${id}`);
+  }
+
+  find(query, by = "name", page = 0) {
+    return http.get(`posts?${by}=${query}&page=${page}`);
+  }
+
+  createPost() {
+    return http.post("/post");
+  }
+
+  publishToggle(data) {
+    return http.post("/publish", data);
+  }
+
+  uploadCoverImage(data) {
+    return http.post("/upload-cover", data);
+  }
+
+  updatePost(data) {
+    return http.put("/post", data);
+  }
+
+  deletePost(data) {
+    return http.delete("/post", {
+      data: { post_id: data.post_id },
+    });
+  }
+
+  getCategories(id) {
+    return http.get(`/categories`);
+  }
+}
+
+export default new PostDataService();
