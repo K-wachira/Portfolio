3224affceeccd2e823405f962f2bf597fae6793e
k-wachira <abkelvinwachira@gmail.com>
2022-10-28 21:41:35 +0300

    https

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL:"http://portfoliobackend-env.eba-ek73xxps.eu-west-2.elasticbeanstalk.com/api/v1/blog",
+    baseURL:"https://portfoliobackend-env.eba-ek73xxps.eu-west-2.elasticbeanstalk.com/api/v1/blog",
     headers: {
         "Content-type": "application/json"
       }
