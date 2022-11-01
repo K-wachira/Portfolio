39b1620dbc67b417ca11ad25f909bece0bd868fe
k-wachira <abkelvinwachira@gmail.com>
2022-11-01 09:51:59 +0300

    Footer cleanup

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -2,10 +2,10 @@ function MyFooter() {
   return (
     <footer class="text-muted py-5 bg-light text-center">
       <div class="container">
-        <p class="float-end mb-1">
+        <p class="float-end">
           <a href="#">Back to top</a>
         </p>
-        <div className="container bg-dark h-100 text-center">
+        <div className="container text-center">
           <button className="pushable">
             <a href="mailto:abkelvinwachira@gmail.com" className="front">
               Lets chat
